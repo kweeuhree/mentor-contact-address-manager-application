e7f9db8a015fc56fd0ec51fdd5f33503784e1772
malikhussain26 <hussain.am2697@gmail.com>
2024-11-26 02:18:55 -0600

    connection to db still not working

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,12 @@ require("dotenv/config");
 
 const Connection = async () => {
     try {
-        console.log(process.env.ATLAS_URI);
+        console.log('ATLAS_URI:', process.env.ATLAS_URI);
         await mongoose.connect(process.env.ATLAS_URI);
         console.log("Database connected successfully");
-    } catch(err) {
+      } catch (err) {
         console.log("Database connection failed", err);
-    }
-}
+      }
+    };
 
-Connection()
+module.exports = Connection;
